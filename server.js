80d9a782ae138b4b0bb6f7ea55b2e6d3123332bb
adarshdebata <adarshdebata00@gmail.com>
2024-09-24 16:35:32 +0530

    server and required package added

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const createRoute = require('./routes/createRoute');
+const readRoute = require('./routes/readRoute');
+const updateRoute = require('./routes/updateRoute');
+const deleteRoute = require('./routes/deleteRoute');
+const connectDB = require('./configs/dbConfig');
+
+require('dotenv').config();
+
+const app = express();
+
+//server Port
+const PORT = process.env.PORT || 3000;
+
+ app.use(express.json());
+
+//middlewares
+
+// Routes
+
+app.use('/tasks', createRoute);
+app.use('/tasks', readRoute);
+app.use('/tasks', updateRoute);
+app.use('/tasks', deleteRoute);
+
+connectDB();
+
+app.listen(PORT, () => {
+console.log('Server is running on port : ',PORT);
+});
